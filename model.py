972ac5f4e1dbebfb94645c30bcde469cf4699e04
Megu-github <80239406+Megu-github@users.noreply.github.com>
2021-10-20 17:55:29 +0900

    MK18_expt.5

--- model.py
+++ model.py
@@ -14,8 +14,12 @@ class CNNs(nn.Module):
         self.conv1 = nn.Conv2d(3,16,3)
         self.conv2 = nn.Conv2d(16,32,3)
         self.conv3 = nn.Conv2d(32,64,3)
-        self.conv4 = nn.Conv2d(64,128,3)
-        self.conv5 = nn.Conv2d(128,256,3)
+        self.conv4 = nn.Conv2d(64,64,3)
+        self.conv5 = nn.Conv2d(64,32,3)
+
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(64)
 
 
         self.dropout1 = nn.Dropout2d(p=0.25)
@@ -27,7 +31,7 @@ class CNNs(nn.Module):
         self.fc3 = nn.Linear(32, 2)
         '''
 
-        self.fc1 = nn.Linear(256 * 5 * 5, 120)
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 2)
         self.fc3 = nn.Linear(32, 2)
 
@@ -35,24 +39,32 @@ class CNNs(nn.Module):
         x = self.conv1(x)
         x = self.relu(x)
         x = self.pool(x)
+
         x = self.conv2(x)
+        x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
 
         x = self.conv3(x)
+        x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
-        x = self.dropout1(x)
+        #x = self.dropout1(x)
+
         x = self.conv4(x)
+        x = self.bn3(x)
         x = self.relu(x)
         x = self.pool(x)
+
         x = self.conv5(x)
         x = self.relu(x)
         x = self.pool(x)
+
         x = x.view(x.size()[0], -1)
+
         x = self.fc1(x)
         x = self.relu(x)
-        x = self.dropout2(x)
+        #x = self.dropout2(x)
         x = self.fc2(x)
         #x = self.relu(x)
         #x = self.fc3(x)
