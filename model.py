a9db213d6f35d7317d8623ffe0c4e153aee272a9
Megu-github <80239406+Megu-github@users.noreply.github.com>
2021-10-26 23:23:29 +0900

    MK18_expt.6_1-code

--- model.py
+++ model.py
@@ -12,18 +12,14 @@ class CNNs(nn.Module):
         self.pool = nn.MaxPool2d(2, stride=2)
 
         
-        '''
+        
         self.conv1 = nn.Conv2d(3,16,3)
         self.conv2 = nn.Conv2d(16,32,3)
         self.conv3 = nn.Conv2d(32,64,3)
         self.conv4 = nn.Conv2d(64,64,3)
         self.conv5 = nn.Conv2d(64,32,3)
-        '''
-        self.conv1 = nn.Conv2d(3,16,3)
-        self.conv2 = nn.Conv2d(16,32,3)
-        self.conv3 = nn.Conv2d(32,32,3)
-        self.conv4 = nn.Conv2d(32,64,3)
-
+        
+        
         self.bn1 = nn.BatchNorm2d(32)
         self.bn2 = nn.BatchNorm2d(64)
         self.bn3 = nn.BatchNorm2d(64)
@@ -38,15 +34,12 @@ class CNNs(nn.Module):
         self.fc3 = nn.Linear(32, 2)
         '''
 
-        '''
+        
         self.fc1 = nn.Linear(32 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 2)
         self.fc3 = nn.Linear(32, 2)
-        '''
+        
 
-        self.fc1 = nn.Linear(64 * 12 * 12, 64*12)
-        self.fc2 = nn.Linear(64*12, 64)
-        self.fc3 = nn.Linear(64, 2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -54,24 +47,24 @@ class CNNs(nn.Module):
         x = self.pool(x)
 
         x = self.conv2(x)
-        #x = self.bn1(x)
+        x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
 
         x = self.conv3(x)
-        #x = self.bn2(x)
+        x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
         #x = self.dropout1(x)
 
         x = self.conv4(x)
-        #x = self.bn3(x)
+        x = self.bn3(x)
         x = self.relu(x)
         x = self.pool(x)
 
-        #x = self.conv5(x)
-        #x = self.relu(x)
-        #x = self.pool(x)
+        x = self.conv5(x)
+        x = self.relu(x)
+        x = self.pool(x)
 
         x = x.view(x.size()[0], -1)
 
@@ -79,7 +72,7 @@ class CNNs(nn.Module):
         x = self.relu(x)
         #x = self.dropout2(x)
         x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
+        #x = self.relu(x)
+        #x = self.fc3(x)
 
         return x
