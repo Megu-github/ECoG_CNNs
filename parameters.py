18749cbca106b81a907b86bf8973d444cfce561c
Megu-github <80239406+Megu-github@users.noreply.github.com>
2021-11-16 18:36:32 +0900

    MK20_expt.6-code

--- parameters.py
+++ parameters.py
@@ -3,7 +3,7 @@ WEIGHT_DECAY = 0.005
 LEARNING_RATE = 0.0001
 RESIZE = [224, 224]
 DEVICE = "cuda" # サーバー上なら"cuda"
-EXPT_NUMBER = 'MK20_expt.5'
+EXPT_NUMBER = 'MK20_expt.6'
 RESULT_DIR_PATH = '/home/megu/ECoG_CNNs/Result/' + EXPT_NUMBER
 
 
