931d6f08dea18c66db13b7fd9d4013470c1904eb
Megu-github <80239406+Megu-github@users.noreply.github.com>
2021-11-26 18:23:41 +0900

    code-review

--- model.py
+++ model.py
@@ -5,13 +5,12 @@ from parameters import RESIZE
 # PytorchでのCNNのモデル作り
 # モデルは次のサイトを参考にした　https://qiita.com/mathlive/items/8e1f）9a8467fff8dfd03c
 class CNNs(nn.Module):
-    def __init__(self):
+    def __init__(self, p_dropout1, p_dropout2, use_Barch_Norm=True):
         super(CNNs, self).__init__()
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
         self.pool = nn.MaxPool2d(2, stride=2)
-
-
+        self.use_Barch_Norm = use_Barch_Norm
 
         self.conv1 = nn.Conv2d(3,16,3, padding=1)
         self.conv2 = nn.Conv2d(16,32,3, padding=1)
@@ -19,14 +18,14 @@ class CNNs(nn.Module):
         self.conv4 = nn.Conv2d(64,64,3, padding=1)
         self.conv5 = nn.Conv2d(64,32,3)
 
+        if self.use_Barch_Norm:
+            self.bn1 = nn.BatchNorm2d(32)
+            self.bn2 = nn.BatchNorm2d(64)
+            self.bn3 = nn.BatchNorm2d(64)
 
-        self.bn1 = nn.BatchNorm2d(32)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.bn3 = nn.BatchNorm2d(64)
 
-
-        self.dropout1 = nn.Dropout2d(p=0.25)
-        self.dropout2 = nn.Dropout2d(p=0.5)
+        self.dropout1 = nn.Dropout2d(p=p_dropout1)
+        self.dropout2 = nn.Dropout2d(p=p_dropout2)
 
         '''
         self.fc1 = nn.Linear(32 * int(RESIZE[0]/4 - 1.5) * int(RESIZE[1]/4 - 1.5), 32*min(int(RESIZE[0]/4 - 1.5), int(RESIZE[1]/4 - 1.5)))
@@ -37,7 +36,7 @@ class CNNs(nn.Module):
 
         self.fc1 = nn.Linear(32 * 6 * 6, 120)
         self.fc2 = nn.Linear(120, 2)
-        self.fc3 = nn.Linear(32, 2)
+        #self.fc3 = nn.Linear(32, 2)
 
 
 
@@ -47,18 +46,21 @@ class CNNs(nn.Module):
         x = self.pool(x)
 
         x = self.conv2(x)
-        #x = self.bn1(x)
+        if self.use_Barch_Norm:
+            x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
 
         x = self.conv3(x)
-        #x = self.bn2(x)
+        if self.use_Barch_Norm:
+            x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
         #x = self.dropout1(x)
 
         x = self.conv4(x)
-        #x = self.bn3(x)
+        if self.use_Barch_Norm:
+            x = self.bn3(x)
         x = self.relu(x)
         x = self.pool(x)
 
